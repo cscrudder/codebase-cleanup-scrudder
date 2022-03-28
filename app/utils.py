53cf5bfa4b511cf2357c19f07dd4e1726b83c98f
cscrudder <cms447@georgetown.edu>
2022-03-28 18:14:56 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -9,3 +9,22 @@ def to_usd(price):
         Invoke like this: to_usd(9.99)
     """
     return '${:,.2f}'.format(price)
+
+
+
+## if this code is in the global scope
+## ... of a file we're trying to import from
+## ... it will throw errors when we try to run those other files
+#price = input("Please choose a price like 4.9999")
+#
+#print(to_usd(float(price)))
+
+if __name__ == "__main__":
+    
+        
+    # if this code is in the global scope
+    # ... of a file we're trying to import from
+    # ... it will throw errors when we try to run those other files
+    price = input("Please choose a price like 4.9999")
+
+    print(to_usd(float(price)))
